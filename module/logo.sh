e440e4beb509a79f969fdaff3b753b279065c968
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-25 11:49:43 +0700

    Create logo.sh

---
+++ module/logo.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#install project
+figlet Install Bahan | lolcat
+sleep 3
+
+pkg update && pkg upgrade
+
+
+figlet selesai | lolcat
+sleep 1
+
+echo HTTPS://XNXX.COM/ |lolcat
+sleep 5
+
+# tampilan
+clear
+echo SABAR YA GESS YAA ● | lolcat
+ sleep 5
+toilet -f big -F gay MTK | lolcat
+echo "=============================================" | lolcat
+echo "[•] Author : MR-Xyaa                         "
+echo "[•] FB     : Xyaa Xyaa                         "
+echo "[•] GitHub : github.com/MR-Xyaa              "
+echo "=============================================" | lolcat
+echo BOCAH GOBLOG |lolcat
+
